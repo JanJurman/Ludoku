9f3efbc75f761805ea6a4126d1d7e8bdc4b6595d
Jan <hencic00>
2017-03-22 14:41:49 +0100

    MongoDb install script

---
+++ mongoDB_install.sh
@@ -0,0 +1,17 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y --allow-unauthenticated mongodb-org
+sudo touch /etc/systemd/system/mongodb.service
+sudo echo "[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+
+[Service]
+User=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/mongodb.service
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
