d755fc0248904a7922052669c2f047ab6c72f773
comemaster <tjaz.vracko@gmail.com>
2017-04-02 18:29:31 +0200

    Add sockets & test

--- socketApi.js
+++ socketApi.js
@@ -7,10 +7,15 @@ socketApi.io = io;
 io.on('connection', function(socket){
     console.log('A user connected');
     socketApi.sendNotification()
+    socket.on('testEmit', function (name, fn) {
+    	console.log("Ime je " + name);
+		fn('woot!!');
+	});
 });
 
 socketApi.sendNotification = function() {
     io.sockets.emit('hello', {msg: 'Hello Tjaž!'});
+
 }
 
 module.exports = socketApi;
