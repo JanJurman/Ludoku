2636b4716e09393f4f76e763d0bdf3713b3ecc8a
comemaster <tjaz.vracko@gmail.com>
2017-04-05 13:14:52 +0200

    Add socket api improvements

---
+++ routes/indexOld.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var fs = require('fs');
+var router = express.Router();
+
+//Load all files in models folder
+fs.readdirSync(__dirname + '/../models').forEach(function(filename)
+{
+	require(__dirname + '/../models/' + filename)
+});
+
+router.get('/', function(req, res, next)
+{
+	res.render('index');
+});
+
+module.exports = router;
